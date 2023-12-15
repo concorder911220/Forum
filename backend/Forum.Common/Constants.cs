8db98e072d5f6978cd9d346a476984dc883ba436
concorder911220 <martinali910205@gmail.com>
2023-12-16 01:29:31 +0200

    better look 2

---
+++ backend/Forum.Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Forum.Common;
+
+public static class Constants
+{
+    public const string AuthUri = "https://accounts.google.com/o/oauth2/auth";
+    public const string TokenUri = "https://oauth2.googleapis.com/token";
+}
---
+++ backend/Forum.Common/Options/GoogleOAuthOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Forum.Common.Options;
+
+public class GoogleOAuthOptions
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+}
